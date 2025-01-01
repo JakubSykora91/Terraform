cc1463941ffca5e66d5b94cd3f47a757c63ec10b
Jakub Sykora <sykora.jakub@centrum.cz>
2025-01-01 14:31:28 +0100

    new

--- createinstance.tf
+++ createinstance.tf
@@ -2,8 +2,8 @@ resource "aws_instance" "MyFirstInstance" {
     count = 3
     ami = "ami-09042b2f6d07d164a"
     instance_type = "t2.micro"
-    
-    tags{
-    Name = "threedemoinstance"
-        }
+
+    tags = {
+      Name = "demoinstnce-${count.index}"
+  }
 }
