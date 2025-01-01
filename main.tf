28fbd0fd0c0f1c7051394ba6d4bfa0163262f6a1
JakubSykora91 <sykora.jakub@centrum.cz>
2025-01-01 14:20:18 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,7 @@
 
 # Configure the AWS Provider
 
-provider "aws" {
-    region = "eu-central-1a"
-    version = "5.82.2"
-
-}
+#provider "aws" {
+ #   region = "eu-central-1a"
+  #  version = "5.82.2"
+#}
