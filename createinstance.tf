2a7ca9d269b296423547f060ef33f5a6b70cd545
Jakub Sykora <sykora.jakub@centrum.cz>
2025-01-03 21:54:43 +0100

    sg in main config

--- createinstance.tf
+++ createinstance.tf
@@ -7,3 +7,21 @@ tags = {
     Name = "demoinstnce-${count.index}"
   }
 }
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = "vpc-05dd1af44c0cbb001"
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = "sg-084ca0331c18d19e1"
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 4443
+  ip_protocol       = "tcp"
+  to_port           = 4443
+}
